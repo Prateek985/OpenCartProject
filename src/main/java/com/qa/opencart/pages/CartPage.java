7664fe1dd0cb70a856b515193a30ab706fa4692a
Prateek985 <prateekchouhan171@yahoo.com>
2024-04-22 20:14:03 +0530

    changes as per prateek review the comments on the cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	int i = 10;
+	private By name = By.id("test");
 	
+	public void add() {
+		System.out.println("This is my add method");
+	}
 }
